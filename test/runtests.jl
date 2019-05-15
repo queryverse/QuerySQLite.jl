b831733559dadb61c52a8a8f998075ade119909d
David Anthoff <anthoff@berkeley.edu>
2019-05-14 20:46:08 -0700

    Add a first test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,16 @@
 using QuerySQLite
+using Query
+using QueryTables
 using Test
 
 @testset "QuerySQLite" begin
 
-@test true
+filename = joinpath(@__DIR__, "Chinook_Sqlite.sqlite")
+
+c = SQLiteConnection(filename)
+
+dt = c.Album |> @filter(_.a > 3) |> DataTable
+
+@test length(dt)==347
 
 end
