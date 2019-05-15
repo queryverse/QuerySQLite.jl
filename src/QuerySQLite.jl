4d835ff4ab7b0135a778ac48081b199e84020ea0
David Anthoff <anthoff@berkeley.edu>
2019-05-14 20:45:53 -0700

    Add initial implementation

--- src/QuerySQLite.jl
+++ src/QuerySQLite.jl
@@ -1,3 +1,39 @@
 module QuerySQLite
 
+import IteratorInterfaceExtensions, QueryOperators, QueryableBackend,
+    TableTraits, SQLite
+
+export SQLiteConnection
+
+struct SQLiteConnection
+    db::SQLite.DB
+end
+
+function SQLiteConnection(file::AbstractString)
+    db = SQLite.DB(file)
+    return SQLiteConnection(db)
+end
+
+struct SQLiteTable
+    conn::SQLiteConnection
+    table::String
+end
+
+IteratorInterfaceExtensions.isiterable(x::SQLiteTable) = true
+TableTraits.isiterabletable(x::SQLiteTable) = true
+IteratorInterfaceExtensions.getiterator(x::SQLiteTable) = SQLite.Query(getfield(x.conn, :db), "SELECT * FROM '$(x.table)';")
+
+Base.getindex(conn::SQLiteConnection, name::AbstractString) = SQLiteTable(conn, name)
+Base.getproperty(conn::SQLiteConnection, name::Symbol) = SQLiteTable(conn, string(name))
+
+function QueryOperators.query(source::SQLiteTable)
+    return QueryableBackend.QueryableSource() do querytree
+
+        # TODO construct the SQL string here by analyzing querytree
+        sql_cmd = "SELECT * FROM '$(source.table)';"
+
+        return SQLite.Query(getfield(source.conn, :db), sql_cmd)
+    end
+end
+
 end # module
